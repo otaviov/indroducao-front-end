f9ee004f3d5b3fa17aed7a2c7f4ae62312139520
Otavio Oliveira <71568294+otaviov@users.noreply.github.com>
2023-01-21 19:54:35 -0300

    up

--- Js/home.js
+++ Js/home.js
@@ -13,11 +13,34 @@ function validaBusca() {
 document.querySelector('#form-busca').onsubmit = function () {
     if (document.querySelector('#q').value == '') {
         document.querySelector('#form-busca').style.background = 'red';
-        document.getElementById('#form-busca').style.backgroundColor = 'Green'
         return false;
     }
-};
+}
 
 // fazendo a associação da função com o evento
 document.querySelector('#form-busca').onsubmit = validaBusca;
-document.querySelector('#form-busca').onfocus = validaBusca;
+
+// Banner Rotativo
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+//trocar a imagem a cada 4 segundos (o js mede tempo em MILÉSIMOS)
+//a variável controle adicionará um botão de pause ao banner
+var timer = setInterval(trocaBanner, 4000);
+var controle = document.querySelector('.pause');
+
+controle.onclick = function () {
+    if (controle.className === 'pause') {
+        clearInterval(timer);
+        controle.className = 'play';
+    } else {
+        timer = setInterval(trocaBanner, 4000);
+        controle.className = 'pause';
+    }
+
+    return false;
+};
